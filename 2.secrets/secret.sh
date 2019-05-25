5b7bbe717421765a202bd7a61c56abb5ec8c55f7
Renan Aragão <renanferreiradearagao@yahoo.com.br>
2019-05-25 14:35:28 -0300

    criação de pods

---
+++ 2.secrets/secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry acr-credentials \
+    --docker-server=refimagens.azurecr.io \
+    --docker-username=refimagens \
+    --docker-password=42/HMspgt/kv2jrqEUcbTQr2h86OJ4+Z
+    --docker-email=refaragao@gmail.com
--- Anotacoes.ps1
+++ Anotacoes.ps1
@@ -21,7 +21,7 @@ az acr login --name refimagens
 az acr list --resource-group k8s-curso --output table
 docker tag renanaragao/api-herois:v1 refimagens.azurecr.io/api-herois:v1
 docker push refimagens.azurecr.io/api-herois:v1
-az acr credential show -n refimagens --query passwords[0].value
+az acr credential show -n refimagens --query "passwords[0].value"
 
 ### Contianer Services
 az container create --resource-group k8s-curso `
@@ -51,3 +51,37 @@ az aks get-credentials --resource-group k8s-curso --name refcluster
 kubectl get nodes
 kubectl create clusterrolebinding kubernetes-dashboard -n kube-system --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
 az aks browse --resource-group k8s-curso --name refcluster
+
+### PODS/Container
+kubectl run mongodb --image mongo:3.5 --port 27017
+kubectl get pods
+kubectl delete pod mongodb-b48794dd4-qhmbg
+kubectl describe pods mongodb-b48794dd4-2hdsl
+kubectl top pod mongodb-b48794dd4-2hdsl ## ver o consumo do pod
+kubectl get pods --output=wide ## achar o IP        
+
+kubectl run api-herois \
+    --image renanaragao/api-herois:v1 \
+    --env "MONGO_URL=10.244.1.3" \
+    --env "PORT=4000" \
+    --replicas 2
+kubectl logs api-herois-5b9d6d7c85-9pp76
+kubectl expose pod api-herois-5b9d6d7c85-9pp76 --port 4000 --type LoadBalancer
+kubectl get services -w
+
+### PODS/Container - Declarativo
+
+kubectl create -f pods/herois-pod.json
+kubectl delete pod api-herois-pod
+kubectl describe pods api-herois-pod  
+kubectl delete pod -l version=v1
+kubectl exec -it api-herois-pod -- /bin/bash ## ou usar o /bin/sh
+kubectl explain pods ## explica o que são as propriedades usadas no aquivo .json ou yaml
+kubectl get pod api-herois-pod -o yaml | grep podIp
+
+kubectl create secret docker-registry acr-credentials \
+    --docker-server=refimagens.azurecr.io \
+    --docker-username=refimagens \
+    --docker-password=42/HMspgt/kv2jrqEUcbTQr2h86OJ4+Z \
+    --docker-email=refaragao@gmail.com
+kubectl get secret
